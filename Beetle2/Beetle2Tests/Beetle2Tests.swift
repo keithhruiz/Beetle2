8fba3f8394469059b5251706033c2197ec15b5ec
keithhruiz <keithhruiz@me.com>
2018-12-05 14:39:28 -0800

    Add files via upload

---
+++ Beetle2/Beetle2Tests/Beetle2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Beetle2Tests.swift
+//  Beetle2Tests
+//
+//  Created by Keith Ruiz-Hopper on 12/5/18.
+//  Copyright © 2018 Keith Ruiz-Hopper. All rights reserved.
+//
+
+import XCTest
+@testable import Beetle2
+
+class Beetle2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
